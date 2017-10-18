3b42197477b8bc28b219da886003bec687b40ac7
Dismes <yuhki.n@gmail.com>
2017-10-17 18:42:38 -0700

    asfd

---
+++ assets/javascripts/database-config.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyA6lz0PLmEMxCcRwS-l5b2ry8Y-YrOWEDw",
+    authDomain: "mmo-fun.firebaseapp.com",
+    databaseURL: "https://mmo-fun.firebaseio.com",
+    projectId: "mmo-fun",
+    storageBucket: "mmo-fun.appspot.com",
+    messagingSenderId: "1084178029398"
+  };
+    firebase.initializeApp(config);
