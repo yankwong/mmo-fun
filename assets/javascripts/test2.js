d690642c2dd2f2e54b8f40303a8461a75d332411
Dismes <yuhki.n@gmail.com>
2017-10-17 19:45:32 -0700

    DONE

--- assets/javascripts/test2.js
+++ assets/javascripts/test2.js
@@ -6,26 +6,50 @@ $("#card5").flip();
 $("#card6").flip();
 $("#card7").flip();
 $("#card8").flip();
-$('.stack').click(function() {
-    
-    $(".card").each(function(e) {
-  
-      setTimeout(function() {
-        $(".card").eq(e).attr("class", "card");
-      }, e * 150)
-      
-    });
-    
+$('.stack').click(function () {
+
+  $(".card").each(function (e) {
+
+    setTimeout(function () {
+      $(".card").eq(e).attr("class", "card");
+    }, e * 150)
+
   });
-  
-  $('.spread').click(function() {
-    
-    $(".card").each(function(e) {
-  
-      setTimeout(function() {
-        $(".card").eq(e).attr("class", "card ani" + e);
-      }, e * 150)
-      
-    });
-    
+
+});
+
+$('.spread').click(function () {
+
+  $(".card").each(function (e) {
+
+    setTimeout(function () {
+      $(".card").eq(e).attr("class", "card ani" + e);
+    }, e * 150)
+
   });
+
+});
+
+$('.shuffle').click(function () {
+  console.log("shuffling");
+
+  $(".card").each(function (e) {
+
+    setTimeout(function () {
+      $(".card").eq(e).attr("class", "card ani" + e);
+    }, e * 150)
+
+  });
+
+  setTimeout(function() {
+    $(".card").each(function (e) {
+      
+          setTimeout(function () {
+            $(".card").eq(e).attr("class", "card");
+          }, e * 150)
+      
+        });
+  }, 500);
+  
+
+});
