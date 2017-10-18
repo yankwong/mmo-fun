304e456d107717c2a057aa9d667a4eb9bff7f516
Dismes <yuhki.n@gmail.com>
2017-10-17 18:18:20 -0700

    shuffle

--- assets/javascripts/test.js
+++ assets/javascripts/test.js
@@ -1,3 +1,36 @@
 console.log("hello");
 $("#card1").flip();
+$("#card2").flip();
+$("#card3").flip();
+$("#card4").flip();
+$("#card5").flip();
+$("#card6").flip();
+$("#card7").flip();
+$("#card8").flip();
+
+
 //https://nnattawat.github.io/flip/ Flip FUnction
+
+$('.stack').click(function() {
+    
+    $(".card").each(function(e) {
+  
+      setTimeout(function() {
+        $(".card").eq(e).attr("class", "card");
+      }, e * 150)
+      
+    });
+    
+  });
+  
+  $('.spread').click(function() {
+    
+    $(".card").each(function(e) {
+  
+      setTimeout(function() {
+        $(".card").eq(e).attr("class", "card ani" + e);
+      }, e * 150)
+      
+    });
+    
+  });
