e5dbcbb701d50758d7154b10f26e3e62fd75f26b
Dismes <yuhki.n@gmail.com>
2017-10-20 01:58:18 -0700

    card moving testo

--- assets/dist/app.js
+++ assets/dist/app.js
@@ -1,3 +1,48 @@
+
+
+$('.stack').click(function () {
+    
+      $(".cardflip").each(function (e) {
+    
+        setTimeout(function () {
+          $(".card").eq(e).attr("class", "card");
+        }, e * 150)
+    
+      });
+    
+    });
+    
+    $('.spread').click(function () {
+    
+      $(".card").each(function (e) {
+    
+        setTimeout(function () {
+          $(".card").eq(e).attr("class", "card ani" + e);
+        }, e * 150)
+    
+      });
+    
+    });
+    
+    $('.shuffle').click(function () {
+      console.log("shuffling");
+    
+      $(".card").each(function (e) {
+    
+        setTimeout(function () {
+          $(".card").eq(e).attr("class", "card ani" + e);
+        }, e * 150)
+    
+      });
+    
+      setTimeout(function () {
+        $(".card").each(function (e) {
+          setTimeout(function () {
+            $(".card").eq(e).attr("class", "card");
+          }, e * 150)
+        });
+      }, 500);
+    });
 $(function() {
   // animation that affect the entire programs goes here
   
